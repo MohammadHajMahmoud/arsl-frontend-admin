47de4e5cdcc36bf9549a304deab75e698edab9a5
MohammadHajMahmoud <mohammdhasaeen@yahoo.com>
2023-05-15 23:48:31 +0300

    validate

--- src/components/Validate.jsx
+++ src/components/Validate.jsx
@@ -1,32 +1,82 @@
-import React, { useEffect,useState } from "react";
-import {useParams,} from "react-router-dom";
-import axios from "axios";
-function Validate(){
-    const [videoUrl, setVideo] = useState("");
-    let {action} = useParams();
-    useEffect(() => {
-        axios.defaults.withCredentials = true;
-        axios.get(`http://localhost:8080/actions/${action}/demo`)
-          .then((response) => {
-                console.log(response)
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }, []);
-  
-    return (
-      <div>
-        {videoUrl ? (
-          <video controls>
-            <source src={videoUrl} type="video/mp4" />
-            {/* Add additional source elements for different video formats if needed */}
-          </video>
-        ) : (
-          <p>Loading video...</p>
-        )}
-      </div>
-    );
-  };
+import React, { useEffect, useState,useRef } from "react";
+import { useParams } from "react-router-dom";
+import {Holistic} from "@mediapipe/holistic";
 
-export default Validate
+function Validate() {
+  const [blob, setBlobData] = useState("");
+  let { action } = useParams();
+  const videoRef = useRef(null);
+  useEffect(() => {
+
+    const holistic = new Holistic({
+        locateFile: (file) => {
+          return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`
+        },
+      })
+
+      holistic.setOptions({
+        upperBodyOnly: true,
+        smoothLandmarks: false,
+        minDetectionConfidence: 0.7,
+        minTrackingConfidence: 0.7,
+      })
+
+      holistic.onResults(onResults)
+      function onResults(results){
+        console.log(results)
+        console.log("hi")
+      }
+              holistic.send({
+                image: videoRef.current
+              });
+
+         
+ },[])
+   
+      
+   
+  useEffect(() => {
+    async function fetchBlobData() {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/actions/${action}/demo`
+        );
+
+        if (!response.ok) {
+          throw new Error("Error retrieving blob data");
+        }
+
+        const blob = await response.blob();
+
+        setBlobData(URL.createObjectURL(blob));
+      } catch (error) {
+        console.error("Error retrieving blob data:", error);
+      }
+    }
+    fetchBlobData();
+
+
+  }, []);
+  function approveVideo() {
+
+  }
+
+ 
+  
+  
+  
+  return (
+    <div>
+      {blob ? (
+        <video id="vod" ref={videoRef} controls>
+          <source src={blob} type="video/mp4" />
+        </video>
+      ) : (
+        <p>Loading video...</p>
+      )}
+      <button></button>
+    </div>
+  );
+}
+
+export default Validate;
