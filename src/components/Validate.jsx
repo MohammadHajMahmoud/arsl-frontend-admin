f2acd2f34eff42e499cbec68f9f6b75ec669a9b1
moawyah <moaywah@cotede.co>
2023-05-16 19:31:22 +0300

    one commit

--- src/components/Validate.jsx
+++ src/components/Validate.jsx
@@ -1,45 +1,52 @@
-
 import { useEffect, useRef, useState } from "react";
 import HolisticModel from "./HolisticModel";
 import { useParams } from "react-router";
+import { persistenceApi } from "./apis";
 
 function Validate(){
   const [video, setVideo] = useState(null);
   const [videoType, setVideoType] = useState(null);
   const { action } = useParams();
   const videoRef = useRef(null);
+  const FRAME_RATE = 15;
+  let handleFrameUpdate = null;
+
+
+  async function fetchVideo(action){
+    const response = await persistenceApi.get(`/actions/${action}/videos/single`, { responseType: 'blob' })
+    setVideo(URL.createObjectURL(response.data));
+    setVideoType(response.headers.get("Content-Type"));
+  }
+  async function setup(action){
+    await fetchVideo(action);
+    
+    while(videoRef.current);
+
+    videoRef.current.addEventListener('timeupdate', handleFrameUpdate);  
+  }
 
     useEffect(() => {
-      
-      fetch(`http://localhost:8080/actions/${action}/videos/single`)
-        .then( (response) => {
-          const blob = response.blob();
-          setVideoType(response.headers.get("Content-Type"));
-          return blob;
-        })
-        .then( (blob) => {
-          setVideo(blob);
-        });
+      setup(action);
 
-      
-      // const holistic = HolisticModel(() => { });
-  
-      // holistic.send({
-      //   image: videoRef.current
-      // });
-    
-      // return () => {
-      //   holistic.close();
-      // };
+      // const holistic = HolisticModel(console.log);
+
+
+      return () => {
+        // holistic.close();
+        videoRef.current.removeEventListener('timeupdate', handleFrameUpdate);
+      };
    }, []);
    
    return (
     <div>
-      <video autoPlay>
+      {video? 
+      <video ref={videoRef}
+      autoPlay muted >
           <source src={video} type={videoType} />
           Your browser does not support the video tag.
-      </video>
-
+      </video>:
+      <p>Loading video...</p>
+      }
       <button>
         valid
       </button>
