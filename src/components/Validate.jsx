099a7b21b38e696437774c26bfd7f7d3460ebd22
moawyah <moaywah@cotede.co>
2023-05-18 16:47:39 +0300

    basic functionality done

--- src/components/Validate.jsx
+++ src/components/Validate.jsx
@@ -16,20 +16,38 @@ function Validate(){
   const rawFrames = [];
   const resultFrames = [];
   const canvas = canvasRef.current;
+  let videoKey = null;
+
+  const onValidation = async () => {
+    await persistenceApi.put(`/actions/${action}/videos/${videoKey}/approve`, {
+      frames: resultFrames
+    });
+    videoKey = null;
+    await fetchVideo(action);
+  }
+
+  const onRejection = async () => {
+    await persistenceApi.put(`/actions/${action}/videos/${videoKey}/reject`);
+    videoKey = null;
+    await fetchVideo(action);
+  }
 
   const holistic = HolisticModel( (result) => {
     resultFrames.push(formatResult(result));
   });
 
-  
   let interval;
   const onPlay = () => {
+    if( resultFrames.length >= 30 ) {
+      return;
+    }
+    resultFrames.length=0;
+    videoElement.play();
     let context;
     if(canvas) {
       context = canvas.getContext('2d');
     }
     interval = setInterval(() => { 
-      videoElement.play();
       context.drawImage(videoElement, 0, 0);
       const imageData = context.getImageData(0, 0, videoElement.videoWidth, videoElement.videoHeight);
       rawFrames.push(imageData);
@@ -42,8 +60,8 @@ function Validate(){
     for(const imageData of rawFrames){
       await holistic.send({ image: imageData })
     }
-    console.log(rawFrames);
-    console.log(resultFrames);
+    rawFrames.length = 0;
+    resultFrames.splice(0, resultFrames.length-30);
   };
 
   async function fetchVideo(action) {
@@ -51,6 +69,7 @@ function Validate(){
     setVideoType(response.headers.get("Content-Type"));
     setVideo(URL.createObjectURL(response.data));
     setVideoElement(document.createElement('video'));
+    videoKey = response.headers.get('video-key'); // FIX:: this is null. findout why and solve it plz.
   }
   
   useEffect(() => {
@@ -69,7 +88,6 @@ function Validate(){
 
       videoElement.src = video;
       videoElement.load();
-      // videoRef.current.play(); todo make it play at the right time
     }
     return () => {
       if(videoRef.current){  
@@ -84,19 +102,19 @@ function Validate(){
     <div>
       {video? 
       <video ref={videoRef}
-      muted controls>
+      muted controls autoPlay>
           <source src={video} type={videoType} />
           Your browser does not support the video tag.
       </video>:
       <p>Loading video...</p>
       }
-      <canvas ref={canvasRef}  />
-      <button>
-        valid
+      <canvas ref={canvasRef} style={{display: 'none'}} width={1000} height={1000}/>
+      <button onClick={onValidation}>
+        Validate
       </button>
 
-      <button>
-        reject
+      <button onClick={onRejection}>
+        Reject
       </button>
     </div>
   );
